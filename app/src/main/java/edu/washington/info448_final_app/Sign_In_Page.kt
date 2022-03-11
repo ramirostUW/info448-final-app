d79316c5363182155657113f29934e4a0c3e6622
Abdiwahid Bishar Hajir <76221708+Superme-Abdiwahid@users.noreply.github.com>
2022-03-10 21:40:08 -0800

    Some more edits

--- app/src/main/java/edu/washington/info448_final_app/Sign_In_Page.kt
+++ app/src/main/java/edu/washington/info448_final_app/Sign_In_Page.kt
@@ -22,6 +22,7 @@ import com.google.firebase.ktx.Firebase
 import com.google.android.gms.tasks.OnCompleteListener
 
 
+
 class Sign_In_Page : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
